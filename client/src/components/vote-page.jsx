83dc346361f256eb8254464fb0de1327aa6166e5
Mori Calisir <104592697+calisirmori@users.noreply.github.com>
2023-09-13 17:06:08 -0700

    home page community averages

--- client/src/components/vote-page.jsx
+++ client/src/components/vote-page.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 function VotePage() {
   const [currentScoutPercent, setCurrentScoutPercent] = useState(50);
@@ -10,13 +11,79 @@ function VotePage() {
   const [currentMedicPercent, setCurrentMedicPercent] = useState(50);
   const [currentSniperPercent, setCurrentSniperPercent] = useState(50);
   const [currentSpyPercent, setCurrentSpyPercent] = useState(50);
+  const [playerAlreadyVoted, setCurrentVoteStatus] = useState(true);
+  const [communityAverage, setCommunityAverages] = useState({});
 
-  const currentUserID = "76561198068401396"
-  const currentMatchID = "1"
+  const currentUserID = "76561198068401396";
+  const currentMatchID = "69";
+
+  const sendDataToServer = async () => {
+    const data = {
+      playerID: currentUserID,
+      matchID: currentMatchID,
+      scout: currentScoutPercent,
+      soldier: currentSoldierPercent,
+      pyro: currentPyroPercent,
+      demoman: currentDemoPercent,
+      heavy: currentHeavyPercent,
+      engineer: currentEngineerPercent,
+      medic: currentMedicPercent,
+      sniper: currentSniperPercent,
+      spy: currentSpyPercent,
+      pointsEarned: 0,
+    };
+
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/api/testwrite",
+        data
+      );
+      console.log("Server Response:", response.data);
+    } catch (error) {
+      console.error("Error posting data:", error);
+    }
+  };
+
+  const params = {
+    userid: currentUserID,
+    matchid: currentMatchID,
+  };
 
   useEffect(() => {
-    console.log(currentScoutPercent);
-  }, [currentScoutPercent]);
+    axios
+      .get("http://localhost:3000/api/check-vote", { params })
+      .then((response) => {
+        response.data[0] === undefined
+          ? setCurrentVoteStatus(false)
+          : setCurrentVoteStatus(true);
+        if (response.data[0] === undefined) {
+        } else {
+          axios
+            .get("http://localhost:3000/api/community-average", { params })
+            .then((response) => {
+              if(communityAverage.scout === undefined) {
+                setCommunityAverages(response.data[0]);
+              }
+            })
+            .catch((error) => {
+              console.error("Error fetching data:", error);
+            });
+          setCurrentScoutPercent(response.data[0].scout_prediction);
+          setCurrentSoldierPercent(response.data[0].soldier_prediction);
+          setCurrentPyroPercent(response.data[0].pyro_prediction);
+          setCurrentDemoPercent(response.data[0].demom_prediction);
+          setCurrentHeavyPercent(response.data[0].heavy_prediction);
+          setCurrentEngineerPercent(response.data[0].engie_prediction);
+          setCurrentMedicPercent(response.data[0].medic_prediction);
+          setCurrentSniperPercent(response.data[0].sniper_prediction);
+          setCurrentSpyPercent(response.data[0].spy_prediction);
+          console.log(communityAverage)
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, [communityAverage]);
 
   const redVote = (currentPercent, setPercentFunction) => {
     const newScore = Math.min(currentPercent + 5, 100);
@@ -54,34 +121,51 @@ function VotePage() {
   };
 
   return (
-    <div class="relative">
+    <div className="relative">
       <img
         src="https://img.maxofs2d.net/source/tf_loghouse_alpine_1920.jpg"
         className=" object-cover w-screen h-screen absolute"
         alt=""
       />
-      <div class=" bg-stone-950 w-screen h-screen absolute opacity-90"></div>
-      <div class="w-screen h-screen absolute font-mont">
+      <div className=" bg-stone-950 w-screen h-screen absolute opacity-90"></div>
+      <div className="w-screen h-screen absolute font-mont">
         <div className=" text-3xl font-bold p-3 pl-6 bg-stone-950 bg-opacity-40 text-stone-200 mb-5 justify-between flex">
-          <div>canyon.tf</div> 
+          <div>canyon.tf</div>
           <a href="http://localhost:3000/api/myprofile">LOGIN</a>
         </div>
         <div className="flex items-center justify-center font-mont  font-bold text-stone-200 mb-5">
           <div className="w-96 flex justify-end text-5xl">FASTFOURTH</div>
           <div className="w-72 flex justify-center text-3xl">VS</div>
-          <div className="w-96 flex justify-start text-5xl wra">SOMEBODY HELP</div>
+          <div className="w-96 flex justify-start text-5xl wra">
+            SOMEBODY HELP
+          </div>
         </div>
-        {classSection(currentScoutPercent, setCurrentScoutPercent, "scout")}
-        {classSection(currentSoldierPercent, setCurrentSoldierPercent, "soldier")}
-        {classSection(currentPyroPercent, setCurrentPyroPercent, "pyro")}
-        {classSection(currentDemoPercent, setCurrentDemoPercent, "demoman")}
-        {classSection(currentHeavyPercent, setCurrentHeavyPercent, "heavy")}
-        {classSection(currentEngineerPercent, setCurrentEngineerPercent, "engie")}
-        {classSection(currentMedicPercent, setCurrentMedicPercent, "medic")}
-        {classSection(currentSniperPercent, setCurrentSniperPercent, "sniper")}
-        {classSection(currentSpyPercent, setCurrentSpyPercent, "spy")}
+        {classSection(currentScoutPercent, setCurrentScoutPercent, "scout", "scout")}
+        {classSection(
+          currentSoldierPercent,
+          setCurrentSoldierPercent,
+          "soldier", "soldier"
+        )}
+        {classSection(currentPyroPercent, setCurrentPyroPercent, "pyro", "pyro")}
+        {classSection(currentDemoPercent, setCurrentDemoPercent, "demoman", "demo")}
+        {classSection(currentHeavyPercent, setCurrentHeavyPercent, "heavy", "heavy")}
+        {classSection(
+          currentEngineerPercent,
+          setCurrentEngineerPercent,
+          "engie", "engi"
+        )}
+        {classSection(currentMedicPercent, setCurrentMedicPercent, "medic", "medic")}
+        {classSection(currentSniperPercent, setCurrentSniperPercent, "sniper", "sniper")}
+        {classSection(currentSpyPercent, setCurrentSpyPercent, "spy", "spy")}
         <div className="flex justify-center items-center mt-6">
-          <div className="select-none font-5xl flex justify-center items-center bg-tf-orange w-40 rounded-md border-2 p-1 border-tf-orange-dark font-bold text-xl text-stone-900 hover:scale-105 cursor-pointer duration-150">SUBMIT</div>
+          {!playerAlreadyVoted && (
+            <div
+              onClick={() => sendDataToServer()}
+              className="select-none font-5xl flex justify-center items-center bg-tf-orange w-40 rounded-md border-2 p-1 border-tf-orange-dark font-bold text-xl text-stone-900 hover:scale-105 cursor-pointer duration-150"
+            >
+              SUBMIT
+            </div>
+          )}
         </div>
       </div>
     </div>
@@ -90,16 +174,21 @@ function VotePage() {
   function classSection(
     currentPercentSelection,
     setCurrentPercentSelection,
-    className
+    className,
+    dbName
   ) {
     return (
       <div className=" flex w-full h-fit justify-center items-center my-3">
         <div className="flex items-center justify-center">
           <div
-            onClick={() =>
-              redVote(currentPercentSelection, setCurrentPercentSelection)
-            }
-            className="h-14 bg-stone-300 w-64 rounded-lg drop-shadow-md hover:cursor-pointer border-4 border-tf-red hover:border-tf-red-dark duration-150 flex items-center justify-center font-bold select-none"
+            onClick={() => {
+              !playerAlreadyVoted &&
+                redVote(currentPercentSelection, setCurrentPercentSelection);
+            }}
+            className={`h-14 bg-stone-300 w-64 rounded-lg drop-shadow-md ${
+              !playerAlreadyVoted &&
+              "hover:cursor-pointer hover:border-tf-red-dark "
+            } border-4 border-tf-red duration-150 flex items-center justify-center font-bold select-none`}
           >
             {currentPlayers.red[className]}
           </div>
@@ -108,29 +197,46 @@ function VotePage() {
               <div className="my-1 relative mx-6">
                 <img
                   src={`../../../classIcons/${className}.png`}
-                  className="h-7 bottom-1.5 -ml-3  absolute transform duration-150 select-none"
+                  className={`h-7 bottom-1.5 -ml-3  absolute transform ${
+                    playerAlreadyVoted ? "duration-1000" : "duration-150"
+                  } select-none`}
                   style={{ left: `${currentPercentSelection - 1}% ` }}
                   alt=""
                 />
                 <div
-                  className="h-2.5 rounded-lg w-1 flex justify-center items-center bg-stone-300 absolute -bottom-1 left-1/2 transform duration-150"
+                  className={`h-2.5 rounded-lg w-1 flex justify-center items-center bg-stone-300 absolute -bottom-1 left-1/2 transform ${
+                    playerAlreadyVoted ? "duration-1000" : "duration-150"
+                  }`}
                   style={{ left: `${currentPercentSelection - 1}%` }}
                 ></div>
+                {playerAlreadyVoted && (
+                  <div
+                    className={`h-2.5 rounded-lg w-1 flex justify-center items-center bg-tf-orange absolute -bottom-1 left-1/2 transform ${
+                      playerAlreadyVoted ? "duration-1000" : "duration-150"
+                    }`}
+                    style={{ left: `${communityAverage[dbName]}%` }}
+                  ></div>
+                )}
               </div>
-
             </div>
             <div className="mx-6 h-1 w-60 bg-tf-blue rounded-md relative">
               <div
-                className="absolute h-1 bg-tf-red duration-150"
+                className={`absolute rounded-l-lg h-1 bg-tf-red ${
+                  playerAlreadyVoted ? "duration-1000" : "duration-150"
+                }`}
                 style={{ width: `${currentPercentSelection}%` }}
               ></div>
             </div>
           </div>
           <div
-            onClick={() =>
-              blueVote(currentPercentSelection, setCurrentPercentSelection)
-            }
-            className="h-14  bg-stone-300 w-64 rounded-lg drop-shadow-md hover:cursor-pointer border-4 border-tf-blue hover:border-tf-blue-dark duration-150 flex items-center justify-center font-bold select-none"
+            onClick={() => {
+              !playerAlreadyVoted &&
+                blueVote(currentPercentSelection, setCurrentPercentSelection);
+            }}
+            className={`h-14  bg-stone-300 w-64 rounded-lg drop-shadow-md ${
+              !playerAlreadyVoted &&
+              "hover:cursor-pointer hover:border-tf-blue-dark"
+            } border-4 border-tf-blue  duration-150 flex items-center justify-center font-bold select-none`}
           >
             {currentPlayers.blue[className]}
           </div>
